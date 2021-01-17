b8061709621c4ef34772a7c99634be08f1b2e736
amarkevich <shade.jdm@yandex.ru>
2021-01-17 18:29:11 +0300

    Add templates

--- news/views.py
+++ news/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import News
+
 def index(request):
-    return HttpResponse('sss')
+    news = News.objects.order_by('-created_at')
+    context = {'news': news, 'title': 'Список новостей'}
+    return render(request, 'news/index.html', context)
