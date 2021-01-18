1f9aca4ae6657cc6c070d4c515e8673a295effd0
amarkevich <shade.jdm@yandex.ru>
2021-01-18 21:43:08 +0300

    Admin tool, model category, index.html

--- news/views.py
+++ news/views.py
@@ -4,6 +4,6 @@ from django.http import HttpResponse
 from .models import News
 
 def index(request):
-    news = News.objects.order_by('-created_at')
+    news = News.objects.all()
     context = {'news': news, 'title': 'Список новостей'}
     return render(request, 'news/index.html', context)
